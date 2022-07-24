1c5d70d4db36ca9e7946b3ca256792c1f338bca2
khamrakulov <khamrakulovkamron@gmail.com>
2022-07-25 00:53:12 +0500

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const { Telegraf } = require('telegraf')
+const { Telegraf } = require('telegraf');
 const bot = new Telegraf("5278678084:AAFrgy4RUcbv_gI5lH0fydL5whpI4f2O3n8")
 
-const admin_id1 = "2111350581";
+const admin_id = 2111350581;
 
 bot.start((ctx) => {
     const first_name = ctx.message.chat.id;

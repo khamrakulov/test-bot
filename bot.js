b35a6d5216009c71452f934d6f70d881e3e5aff8
khamrakulov <khamrakulovkamron@gmail.com>
2022-07-24 23:53:25 +0500

    update

--- bot.js
+++ bot.js
@@ -4,6 +4,15 @@ const bot = new Telegraf(process.env.BOT_TOKEN)
 bot.telegram.setWebhook('https://test-bot-xfeusw.herokuapp.com');
 bot.startWebhook(`/`, null, 4000);
 
+bot.use(function(ctx, next){
+    try{
+        if(ctx.chat == undefined) return;
+        console.log("Hello World");
+    }catch (e){
+        console.log("Error");
+    }
+});
+
 bot.start((ctx) => ctx.reply('Welcome'))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on('sticker', (ctx) => ctx.reply('👍'))

153fec7f7530c9988f5db4c679d8a6670628412c
khamrakulov <khamrakulovkamron@gmail.com>
2022-07-25 00:51:11 +0500

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Telegraf } = require('telegraf')
 const bot = new Telegraf("5278678084:AAFrgy4RUcbv_gI5lH0fydL5whpI4f2O3n8")
 
-const admin_id = "2111350581"
+const admin_id1 = "2111350581";
 
 bot.start((ctx) => {
     const first_name = ctx.message.chat.id;
@@ -15,7 +15,8 @@ bot.hears(/./, ctx => {
 
     console.log(ctx)
 
-    if (chat_id == admin_id && ctx.message.reply_to_message){
+    if ((chat_id == admin_id) && (ctx.message.reply_to_message)){
+        console.log('Reply keldi, birad!')
         const reply_chat_id = ctx.message.reply_to_message.chat.id;
         ctx.sendMessage(reply_chat_id, text);
     }else{

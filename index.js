dfebb41d8f2e5bf09f81b310a335d7a50b8addd0
khamrakulov <khamrakulovkamron@gmail.com>
2022-07-25 00:11:49 +0500

    update

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const { Telegraf } = require('telegraf')
-const index = new Telegraf(process.env.BOT_TOKEN)
+const bot = new Telegraf("5278678084:AAFrgy4RUcbv_gI5lH0fydL5whpI4f2O3n8")
 
-index.telegram.setWebhook('https://test-bot-xfeusw.herokuapp.com');
-index.startWebhook(`/`, null, 4000);
+// bot.telegram.setWebhook('https://test-bot-xfeusw.herokuapp.com');
+// bot.startWebhook(`/`, null, 4000);h
+//
+// bot.use(function(ctx, next){
+//     try{
+//         if(ctx.chat == undefined) return;
+//         console.log("Hello World");
+//     }catch (e){
+//         console.log("Error");
+//     }
+// });
 
-index.use(function(ctx, next){
-    try{
-        if(ctx.chat == undefined) return;
-        console.log("Hello World");
-    }catch (e){
-        console.log("Error");
-    }
-});
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
 
-index.start((ctx) => ctx.reply('Welcome'))
-index.help((ctx) => ctx.reply('Send me a sticker'))
-index.on('sticker', (ctx) => ctx.reply('👍'))
-index.hears('hi', (ctx) => ctx.reply('Hey there'))
-
-index.launch();
+bot.launch();

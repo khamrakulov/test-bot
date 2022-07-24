4e15d537069660fbb5c801ffa7b06cab50cc2459
khamrakulov <khamrakulovkamron@gmail.com>
2022-07-25 00:34:55 +0500

    update

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
 const { Telegraf } = require('telegraf')
 const bot = new Telegraf("5278678084:AAFrgy4RUcbv_gI5lH0fydL5whpI4f2O3n8")
 
-// bot.telegram.setWebhook('https://test-bot-xfeusw.herokuapp.com');
-// bot.startWebhook(`/`, null, 4000);h
-//
-// bot.use(function(ctx, next){
-//     try{
-//         if(ctx.chat == undefined) return;
-//         console.log("Hello World");
-//     }catch (e){
-//         console.log("Error");
-//     }
-// });
+const admin_id = "2111350581"
 
-bot.start((ctx) => ctx.reply(`Welcome, Kamron!`))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.on('sticker', (ctx) => ctx.reply('👍'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-bot.hears(/./, ctx => ctx.reply("Any symbol!"))
+bot.start((ctx) => {
+    const first_name = ctx.message.chat.id;
+    ctx.reply(`Welcome, ${first_name}`);
+})
+bot.help((ctx) => ctx.reply('This bot help you for connecting with Kamron Khamrakulov'))
+bot.hears(/./, ctx => {
+    const chat_id = ctx.message.chat.id;
+    const message_id = ctx.message.message_id;
+    ctx.forwardMessage(admin_id, chat_id, false, false, message_id);
+})
 
 bot.launch();

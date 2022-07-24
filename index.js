237ec80d14284c836ebe32bc8ecf9ab7d14bfc6f
khamrakulov <khamrakulovkamron@gmail.com>
2022-07-25 01:05:24 +0500

    update

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bot = new Telegraf("5278678084:AAFrgy4RUcbv_gI5lH0fydL5whpI4f2O3n8")
 const admin_id = 2111350581;
 
 bot.start((ctx) => {
-    const first_name = ctx.message.chat.id;
+    const first_name = ctx.message.chat.first_name;
     ctx.reply(`Welcome, ${first_name}`);
 })
 bot.help((ctx) => ctx.reply('This bot help you for connecting with Kamron Khamrakulov'))
@@ -23,5 +23,3 @@ bot.hears(/./, ctx => {
         ctx.forwardMessage(admin_id, chat_id, false, false, message_id);
     }
 })
-
-bot.launch();
